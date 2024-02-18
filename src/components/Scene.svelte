4a72df5966363f7333793e80f20dffedc6cbd2a2
Christian Lee <christianjslee@gmail.com>
2024-02-18 16:59:17 -0500

    Remove 3d link

--- src/components/Scene.svelte
+++ src/components/Scene.svelte
@@ -1,44 +1,25 @@
 <script lang="ts">
-  import { goto } from '$app/navigation';
-  import { spring } from 'svelte/motion';
   import { T, useTask } from '@threlte/core';
-  import { OrbitControls, interactivity, useCursor } from '@threlte/extras';
+  import { OrbitControls } from '@threlte/extras';
   import { PlaneGeometry} from 'three';
   import { FontLoader } from 'three/addons/loaders/FontLoader.js';
   import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
   import { noise3 } from '$utils/perlin.js'; 
 
-  interactivity();
-
-  const { onPointerEnter, onPointerLeave } = useCursor();
   const loader = new FontLoader();
-  const scale = spring(1);
   let logoTextGeometry;
-  let shopTextGeometry;
-  let shopTextColor = 'hotpink';
   loader.load('/helvetiker.typeface.json', (font) => {
     logoTextGeometry = new TextGeometry(
       'perlin noise',
       {
         size: 1,
         font: font,
-        height: 0.2,
+        height: 0.1,
         curveSegments: 2,
         depth: 0.2
       }
     );
-    shopTextGeometry = new TextGeometry(
-      'shop',
-      {
-        size: 1,
-        font: font,
-        height: 0.2,
-        curveSegments: 2,
-        depth: 0.2
-      }
-    )
     logoTextGeometry.computeBoundingBox();
-    shopTextGeometry.computeBoundingBox();
   });
   let geometry = new PlaneGeometry(20, 20, 50, 50);
   let vertices = geometry.getAttribute('position');
@@ -81,7 +62,7 @@
     rotateSpeed={0.15}
     enablePan={false}
     target.y={0}
-    enableZoom={false}
+    enableZoom={true}
   />
 </T.PerspectiveCamera>
 
@@ -100,33 +81,6 @@
   <T.MeshBasicMaterial
     wireframe={true}
     color={"hotpink"}
-  />
-</T.Mesh>
-{/if}
-
-{#if shopTextGeometry}
-<T.Mesh
-  bind:geometry={shopTextGeometry}
-  position={[-10, 0.5, -10]}
-  rotation={[0, -Math.PI / 2, 0]}
-  scale={$scale}
-  on:pointerenter={onPointerEnter}
-  on:pointerleave={onPointerLeave}
-  on:pointerenter={() => {
-    $scale = 1.1;
-    shopTextColor = 'orange';
-  }}
-  on:pointerleave={() => {
-    $scale = 1;
-    shopTextColor = 'hotpink';
-  }}
-  on:click={() => {
-   goto('/search'); 
-  }}
-  >
-  <T.MeshBasicMaterial
-    wireframe={true}
-    color={shopTextColor}
   />
 </T.Mesh>
 {/if}
